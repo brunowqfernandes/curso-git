8e9386a58c0fb27ada935d21eb5ef075b76321dd
Bruno Fernandes <bruno.wqfernandes@gmail.com>
2021-06-19 20:50:23 -0300

    Acrscentada pergunta como voce esta

--- projeto_teste/Program.cs
+++ projeto_teste/Program.cs
@@ -7,7 +7,9 @@ namespace projeto_teste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia!");
+            Console.WriteLine("Bom dia!"); 
+            Console.WriteLine("Como você está hoje?");
+
         }
     }
 }
